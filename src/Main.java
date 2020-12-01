89bd8473048c3c4e64af44ba6876d7ef69ceca91
Stanislav Krausov <krausov@bk.ru>
2020-12-01 14:33:34 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int account = 0;
+        int transfer = 600;
+        int total = account + transfer;
+        System.out.println(total);
+    }
+}
