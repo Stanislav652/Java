da980f979de69c3bbb2760d84dea16b9882f33dc
Stanislav Krausov <krausov@bk.ru>
2020-12-02 22:03:48 +0300

    Added changes 3

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        int account = 2_000_000;
-        int transfer = 500_000;
+        int account = 1_900_000_000;
+        int transfer = 400_000_000;
         int total = account + transfer;
         System.out.println(total);
     }
